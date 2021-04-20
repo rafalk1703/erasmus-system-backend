ae26d3c5ec993ab76f7ae6e6e28b2492f9cc0f06
Rafał Krajewski <rafalk1703@gmail.com>
2021-04-20 16:57:24 +0200

    Initial commit

---
+++ src/main/java/pl/agh/edu/erasmus_system/ErasmusSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.agh.edu.erasmus_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErasmusSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErasmusSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/agh/edu/erasmus_system/ErasmusSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.agh.edu.erasmus_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErasmusSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
