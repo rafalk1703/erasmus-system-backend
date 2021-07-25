6c7f66fea5290ac972abe27c7d7713c9f03a8320
Dawid Kulma <dawidkulma@gmail.com>
2021-07-25 13:22:33 +0200

    Fix small bug

--- src/main/java/pl/agh/edu/erasmus_system/controller/qualification/QualificationController.java
+++ src/main/java/pl/agh/edu/erasmus_system/controller/qualification/QualificationController.java
@@ -3,9 +3,7 @@ package pl.agh.edu.erasmus_system.controller.qualification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.agh.edu.erasmus_system.controller.qualification.response_bodies.*;
 import pl.agh.edu.erasmus_system.model.Contract;
 import pl.agh.edu.erasmus_system.model.Registration;
@@ -19,6 +17,8 @@ import java.util.List;
  * Class contains endpoints for qualification view
  */
 @RestController
+@RequestMapping("api/")
+@CrossOrigin
 public class QualificationController {
 
     @Autowired
