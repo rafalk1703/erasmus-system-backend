e12cba5d40dbf7737ac84dfbdd3b07fc7f401260
Rafał Krajewski <rafalk1703@gmail.com>
2021-08-26 23:41:50 +0200

    generate pdf file from template

---
+++ src/main/java/pl/agh/edu/erasmus_system/controller/GeneratePdfFileController.java
@@ -0,0 +1,41 @@
+package pl.agh.edu.erasmus_system.controller;
+
+import com.lowagie.text.DocumentException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pl.agh.edu.erasmus_system.service.GeneratePdfFileService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("api/file")
+@CrossOrigin
+public class GeneratePdfFileController {
+
+    @Autowired
+    private GeneratePdfFileService generatePdfFileService;
+
+    @RequestMapping(value="/generate", method= RequestMethod.GET)
+    public void downloadPDFResource(HttpServletResponse response) {
+        try {
+            Path file = Paths.get(generatePdfFileService.generatePdf().getAbsolutePath());
+            if (Files.exists(file)) {
+                response.setContentType("application/pdf");
+                response.addHeader("Content-Disposition",
+                        "attachment; filename=" + "Lista_wydzialowa_WIEiT");
+                Files.copy(file, response.getOutputStream());
+                response.getOutputStream().flush();
+            }
+        } catch (DocumentException | IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/pl/agh/edu/erasmus_system/service/GeneratePdfFileService.java
@@ -0,0 +1,62 @@
+package pl.agh.edu.erasmus_system.service;
+
+import com.lowagie.text.DocumentException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+import pl.agh.edu.erasmus_system.repository.RegistrationRepository;
+import pl.agh.edu.erasmus_system.repository.StudentRepository;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Service
+public class GeneratePdfFileService {
+
+    private static final String PDF_RESOURCES = "/templates/";
+
+    @Autowired
+    private RegistrationRepository registrationRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private SpringTemplateEngine templateEngine;
+
+
+    public File generatePdf() throws IOException, DocumentException {
+        Context context = getContext();
+        String html = loadAndFillTemplate(context);
+        return renderPdf(html);
+    }
+
+
+    private File renderPdf(String html) throws IOException, DocumentException {
+        File file = File.createTempFile("students", ".pdf");
+        OutputStream outputStream = new FileOutputStream(file);
+        ITextRenderer renderer = new ITextRenderer(20f * 4f / 3f, 20);
+        renderer.setDocumentFromString(html, new ClassPathResource(PDF_RESOURCES).getURL().toExternalForm());
+        renderer.layout();
+        renderer.createPDF(outputStream);
+        outputStream.close();
+        file.deleteOnExit();
+        return file;
+    }
+
+    private Context getContext() {
+        Context context = new Context();
+        context.setVariable("students", studentRepository.findAll());
+        return context;
+    }
+
+    private String loadAndFillTemplate(Context context) {
+        return templateEngine.process("result_pdf_file", context);
+    }
+
+}
