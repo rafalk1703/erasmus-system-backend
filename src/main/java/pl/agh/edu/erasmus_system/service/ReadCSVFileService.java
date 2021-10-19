24aa601870644952c0882d0182b079a0ad84bf0f
Rafał Krajewski <rafalk1703@gmail.com>
2021-10-19 19:50:19 +0200

    delete useless code

--- src/main/java/pl/agh/edu/erasmus_system/service/ReadCSVFileService.java
+++ src/main/java/pl/agh/edu/erasmus_system/service/ReadCSVFileService.java
@@ -156,127 +156,6 @@ public class ReadCSVFileService {
         }
     }
 
-    public void saveRegistrationsToDatabase(File file, String year) {
-        List<String[]> r = null;
-        try (CSVReader reader = new CSVReader(new FileReader(file))) {
-            r = reader.readAll();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Edition edition = editionRepository.findByYear(year).get();
-
-        int listIndex = 0;
-        for (String[] arrays : r) {
-            if (listIndex++ > 0) {
-                Registration registration = new Registration();
-                Student student = null;
-                if (studentRepository.findStudentByEditionAndEmail(edition.getId(), Array.get(arrays, 7).toString()).isEmpty()) {
-                    student = new Student();
-                    student.setName(Array.get(arrays, 4).toString());
-                    student.setSurname(Array.get(arrays, 5).toString());
-                    student.setDepartment(Array.get(arrays, 3).toString());
-                    student.setEmail(Array.get(arrays, 7).toString());
-                    student.setYear(Array.get(arrays, 2).toString());
-                    student.setField(Array.get(arrays, 22).toString());
-                    student.setPhoneNumber(Array.get(arrays, 23).toString());
-                    student.setEarlierParticipation(Array.get(arrays, 24).toString());
-                    student.setPeriodOfStay(Array.get(arrays, 20).toString());
-                    studentRepository.save(student);
-                } else {
-                    student = studentRepository.findStudentByEditionAndEmail(edition.getId(), Array.get(arrays, 7).toString()).get();
-                }
-                registration.setIsNominated(false);
-                registration.setIsAccepted(false);
-                registration.setStudent(student);
-                registration.setPriority(Integer.parseInt(Array.get(arrays, 21).toString()));
-                ContractsCoordinator contractsCoordinator = contractCoordinatorRepository.findByName(Array.get(arrays, 15).toString()).get();
-                String erasmusCode = Array.get(arrays, 17).toString();
-                String degree = Array.get(arrays, 19).toString() + "st";
-                Contract contract = contractRepository.findByErasmusCodeAndContractsCoordinator_CodeAndEditionAndDegree(erasmusCode, contractsCoordinator.getCode(), edition, degree).get();
-                registration.setContract(contract);
-                registrationRepository.save(registration);
-            }
-
-        }
-    }
-
-    public void updateRegistrations(File file, long id) {
-        List<String[]> r = null;
-        try (CSVReader reader = new CSVReader(new FileReader(file))) {
-            r = reader.readAll();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Edition edition = editionRepository.findById(id).get();
-
-        int listIndex = 0;
-        for (String[] arrays : r) {
-            if (listIndex++ > 0) {
-
-                Registration registration = new Registration();
-                Student student = null;
-                if (studentRepository.findStudentByEditionAndEmail(edition.getId(), Array.get(arrays, 7).toString()).isEmpty()) {
-                    student = new Student();
-                    student.setName(Array.get(arrays, 4).toString());
-                    student.setSurname(Array.get(arrays, 5).toString());
-                    student.setDepartment(Array.get(arrays, 3).toString());
-                    student.setEmail(Array.get(arrays, 7).toString());
-                    student.setYear(Array.get(arrays, 2).toString());
-                    student.setField(Array.get(arrays, 22).toString());
-                    student.setPhoneNumber(Array.get(arrays, 23).toString());
-                    student.setEarlierParticipation(Array.get(arrays, 24).toString());
-                    student.setPeriodOfStay(Array.get(arrays, 20).toString());
-                    studentRepository.save(student);
-                    registration.setIsNominated(false);
-                    registration.setIsAccepted(false);
-                    registration.setStudent(student);
-                    registration.setPriority(Integer.parseInt(Array.get(arrays, 21).toString()));
-                    ContractsCoordinator contractsCoordinator = contractCoordinatorRepository.findByName(Array.get(arrays, 15).toString()).get();
-                    String erasmusCode = Array.get(arrays, 17).toString();
-                    String degree = Array.get(arrays, 19).toString() + "st";
-                    Contract contract = contractRepository.findByErasmusCodeAndContractsCoordinator_CodeAndEditionAndDegree(erasmusCode, contractsCoordinator.getCode(), edition, degree).get();
-                    registration.setContract(contract);
-                    registrationRepository.save(registration);
-                } else {
-                    student = studentRepository.findStudentByEditionAndEmail(edition.getId(), Array.get(arrays, 7).toString()).get();
-                    if (!student.getName().equals(Array.get(arrays, 4).toString()))
-                        student.setName(Array.get(arrays, 4).toString());
-                    if (!student.getSurname().equals(Array.get(arrays, 5).toString()))
-                        student.setSurname(Array.get(arrays, 5).toString());
-                    if (!student.getDepartment().equals(Array.get(arrays, 3).toString()))
-                        student.setDepartment(Array.get(arrays, 3).toString());
-                    if (!student.getYear().equals(Array.get(arrays, 2).toString()))
-                        student.setYear(Array.get(arrays, 2).toString());
-                    if (!student.getField().equals(Array.get(arrays, 22).toString()))
-                        student.setField(Array.get(arrays, 22).toString());
-                    if (!student.getPhoneNumber().equals(Array.get(arrays, 23).toString()))
-                        student.setPhoneNumber(Array.get(arrays, 23).toString());
-                    if (!student.getEarlierParticipation().equals(Array.get(arrays, 24).toString()))
-                        student.setEarlierParticipation(Array.get(arrays, 24).toString());
-                    if (!student.getPeriodOfStay().equals(Array.get(arrays, 20).toString()))
-                        student.setPeriodOfStay(Array.get(arrays, 20).toString());
-                    studentRepository.save(student);
-                    registration = registrationRepository.findByStudent_IdAndPriority(student.getId(), Integer.parseInt(Array.get(arrays, 21).toString())).get();
-
-                    ContractsCoordinator contractsCoordinator = contractCoordinatorRepository.findByName(Array.get(arrays, 15).toString()).get();
-                    String erasmusCode = Array.get(arrays, 17).toString();
-                    String degree = Array.get(arrays, 19).toString() + "st";
-
-                    if (!registration.getContract().getErasmusCode().equals(erasmusCode) ||
-                            !registration.getContract().getErasmusCode().equals(degree) ||
-                            !registration.getContract().getContractsCoordinator().equals(contractsCoordinator)) {
-                        Contract contract = contractRepository.findByErasmusCodeAndContractsCoordinator_CodeAndEditionAndDegree(erasmusCode, contractsCoordinator.getCode(), edition, degree).get();
-                        registration.setContract(contract);
-                        registrationRepository.save(registration);
-                    }
-                }
-
-            }
-
-        }
-    }
-
     public void saveRegistrationsToDatabase2(File file, String year) {
         List<String[]> r = null;
         try (CSVReader reader = new CSVReader(new FileReader(file))) {
@@ -286,8 +165,6 @@ public class ReadCSVFileService {
         }
 
         Edition edition = editionRepository.findByYear(year).get();
-
-        System.out.println("test1");
 
         int listIndex = 0;
 
@@ -312,8 +189,6 @@ public class ReadCSVFileService {
         }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
 
 
-        System.out.println("mapa " + map);
-
         Map<String, Integer> map2 = new HashedMap<>();
         Pattern p = Pattern.compile("\\((\\d+)\\)");
         String [] header = r.get(0);
@@ -322,15 +197,11 @@ public class ReadCSVFileService {
                 Matcher match = p.matcher(header[i]);
                 if (match.find()) {
                     String columnName = map.get(match.group(1));
-                    System.out.println("header = " + header[i]);
-                    System.out.println("column name = " + columnName);
-                    System.out.println("numer z kolumny  = " + match.group(1));
                     map2.put(columnName, i);
                 }
             }
         }
 
-        System.out.println(map2);
         for (String[] arrays : r) {
             if (listIndex++ > 0) {
 
@@ -353,36 +224,24 @@ public class ReadCSVFileService {
                     student.setExamLevel(Array.get(arrays, map2.get("Poziom Egzaminu")).toString());
                     student.setTypeOfCertificate(Array.get(arrays, map2.get("Rodzaj certyfikatu")).toString());
                     student.setForeignLanguage(Array.get(arrays, map2.get("Język")).toString());
-                    System.out.println("USTAWIAM ADRES " + Array.get(arrays, map2.get("Adres")).toString());
                     student.setAddress(Array.get(arrays, map2.get("Adres")).toString());
-                    System.out.println("student adress " + student.getAddress());
                     studentRepository.save(student);
-                    System.out.println("add student " + student.getName());
                 } else {
                     student = studentRepository.findStudentByEditionAndEmail(edition.getId(), Array.get(arrays, map2.get("Email")).toString()).get();
-                    System.out.println("add student2 " + student.getName());
                 }
-                System.out.println(agreements.toString());
                 for (int i = 0; i < agreements.size(); i++) {
-                    System.out.println("agregregrge " + agreements.get(i));
                     if (!agreements.get(i).equals("BRAK")) {
                         Registration registration = new Registration();
                         registration.setIsNominated(false);
                         registration.setIsAccepted(false);
                         registration.setStudent(student);
                         registration.setPriority(i+1);
-                        System.out.println("contractsCoordinator " + getCoordinatorNameFromAgreement(agreements.get(i)));
                         ContractsCoordinator contractsCoordinator = contractCoordinatorRepository
                                 .findByName(getCoordinatorNameFromAgreement(agreements.get(i))).get();
-                        System.out.println("coordinator code " + contractsCoordinator.getCode());
 
-                        System.out.println();
                         String erasmusCode = getErasmusCodeFromAgreement(agreements.get(i));
-                        System.out.println(" erasmus code " + erasmusCode);
                         String degree = getDegreeFromYear(Array.get(arrays, map2.get("Rok")).toString());
-                        System.out.println(" degree code " + degree);
                         Contract contract = contractRepository.findByErasmusCodeAndContractsCoordinator_CodeAndEditionAndDegree(erasmusCode, contractsCoordinator.getCode(), edition, degree).get();
-                        System.out.println(" contract " + contract.getErasmusCode());
                         registration.setContract(contract);
                         if (registrationRepository.findByContractAndStudentAndPriority(registration.getContract(), registration.getStudent(), registration.getPriority()).isEmpty())
                             registrationRepository.save(registration);
@@ -425,8 +284,6 @@ public class ReadCSVFileService {
         }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
 
 
-        System.out.println("mapa " + map);
-
         Map<String, Integer> map2 = new HashedMap<>();
         Pattern p = Pattern.compile("\\((\\d+)\\)");
         String [] header = r.get(0);
@@ -447,11 +304,9 @@ public class ReadCSVFileService {
                         Array.get(arrays, map2.get("Umowa 2")).toString(),
                         Array.get(arrays, map2.get("Umowa 3")).toString());
 
-                System.out.println(agreements.toString());
                 Student student = null;
                 //1 dodanie nowego studenta
                 if (studentRepository.findStudentByEditionAndEmail(edition.getId(), Array.get(arrays, map2.get("Email")).toString()).isEmpty()) {
-                    System.out.println("DODANIE NOWEGO STUDENTA");
                     student = new Student();
                     student.setName(Array.get(arrays, map2.get("Imię")).toString());
                     student.setSurname(Array.get(arrays, map2.get("Nawisko")).toString());
@@ -469,46 +324,30 @@ public class ReadCSVFileService {
                     student.setAddress(Array.get(arrays, map2.get("Adres")).toString());
                     studentRepository.save(student);
                     for (int i = 0; i < agreements.size(); i++) {
-                        System.out.println("agregregrge " + agreements.get(i));
                         if (!agreements.get(i).equals("BRAK")) {
                             Registration registration = new Registration();
                             registration.setIsNominated(false);
                             registration.setIsAccepted(false);
                             registration.setStudent(student);
                             registration.setPriority(i+1);
-                            System.out.println("contractsCoordinator " + getCoordinatorNameFromAgreement(agreements.get(i)));
                             ContractsCoordinator contractsCoordinator = contractCoordinatorRepository
                                     .findByName(getCoordinatorNameFromAgreement(agreements.get(i))).get();
-                            System.out.println("coordinator code " + contractsCoordinator.getCode());
 
-                            System.out.println();
                             String erasmusCode = getErasmusCodeFromAgreement(agreements.get(i));
-                            System.out.println(" erasmus code " + erasmusCode);
                             String degree = getDegreeFromYear(Array.get(arrays, map2.get("Rok")).toString());
-                            System.out.println(" degree code " + degree);
                             Contract contract = contractRepository.findByErasmusCodeAndContractsCoordinator_CodeAndEditionAndDegree(erasmusCode, contractsCoordinator.getCode(), edition, degree).get();
-                            System.out.println(" contract " + contract.getErasmusCode());
                             registration.setContract(contract);
                             if (registrationRepository.findByContractAndStudentAndPriority(registration.getContract(), registration.getStudent(), registration.getPriority()).isEmpty())
                                 registrationRepository.save(registration);
                         }
                     }
                 } else {
-                    System.out.println("STUDENT ISNIJE");
                     // 2 student istnieje ale zmienia dane
                     student = studentRepository.findStudentByEditionAndEmail(edition.getId(), Array.get(arrays, map2.get("Email")).toString()).get();
-                    System.out.println("student znalezniony " + student.getSurname());
-                    if (!student.getName().equals(Array.get(arrays, map2.get("Imię")).toString())){
-                        System.out.println("petla 1");
+                    if (!student.getName().equals(Array.get(arrays, map2.get("Imię")).toString()))
                         student.setName(Array.get(arrays, map2.get("Imię")).toString());
-                    }
-
                     if (!student.getSurname().equals(Array.get(arrays, map2.get("Nawisko")).toString()))
-                    {
-                        System.out.println("przed " + student.getSurname() + " po " + Array.get(arrays, map2.get("Nawisko")).toString());
                         student.setSurname(Array.get(arrays, map2.get("Nawisko")).toString());
-                    }
-
                     if (!student.getDepartment().equals(Array.get(arrays, map2.get("Wydział")).toString()))
                         student.setDepartment(Array.get(arrays, map2.get("Wydział")).toString());
                     if (!student.getYear().equals(Array.get(arrays, map2.get("Rok")).toString()))
@@ -529,23 +368,19 @@ public class ReadCSVFileService {
                         student.setForeignLanguage(Array.get(arrays, map2.get("Język")).toString());
                     if (!student.getAverageGrade().equals(Array.get(arrays, map2.get("Średnia")).toString()))
                         student.setAverageGrade(Array.get(arrays, map2.get("Średnia")).toString());
-                    System.out.println("student przed adres " + student.getSurname());
                     if (!student.getAddress().equals(Array.get(arrays, map2.get("Adres")).toString()))
                         student.setAddress(Array.get(arrays, map2.get("Adres")).toString());
 
-                    System.out.println("student przed zapisanie " + student.getSurname());
                     studentRepository.save(student);
 
                     // 3 student istnieje ale zmienia umowy
                     for (int i = 0; i < agreements.size(); i++) {
                         if (agreements.get(i).equals("BRAK") && registrationRepository.findByStudent_IdAndPriority(student.getId(), i+1).isEmpty()){
-                            System.out.println("pętla 1");
                             continue;
                         }
 
 
                         if (!agreements.get(i).equals("BRAK") && registrationRepository.findByStudent_IdAndPriority(student.getId(), i+1).isEmpty()) {
-                            System.out.println("pętla 2");
                             Registration registration = new Registration();
                             registration.setIsNominated(false);
                             registration.setIsAccepted(false);
@@ -563,13 +398,11 @@ public class ReadCSVFileService {
 
 
                         if (agreements.get(i).equals("BRAK") && registrationRepository.findByStudent_IdAndPriority(student.getId(), i+1).isPresent()) {
-                            System.out.println("pętla 3");
                             Registration registration = registrationRepository.findByStudent_IdAndPriority(student.getId(), i + 1).get();
                             registrationRepository.delete(registration);
                         }
 
                         if (!agreements.get(i).equals("BRAK") && registrationRepository.findByStudent_IdAndPriority(student.getId(), i+1).isPresent()) {
-                            System.out.println("pętla 4");
                             Registration registration = registrationRepository.findByStudent_IdAndPriority(student.getId(), i + 1).get();
 
                             String erasmusCode = registration.getContract().getErasmusCode();
