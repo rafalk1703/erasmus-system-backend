47df5208b9a4dfdd394625fcf6586e53736f657d
Rafał Krajewski <rafalk1703@gmail.com>
2021-11-25 20:01:36 +0100

    add swagger

--- src/main/java/pl/agh/edu/erasmus_system/ErasmusSystemApplication.java
+++ src/main/java/pl/agh/edu/erasmus_system/ErasmusSystemApplication.java
@@ -2,8 +2,10 @@ package pl.agh.edu.erasmus_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ErasmusSystemApplication {
 
     public static void main(String[] args) {
